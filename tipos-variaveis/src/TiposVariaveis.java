1730f89a8ebc6bdc2802bf70a2379c848c94e63a
João Lucas Martiliano da Silva <lucasmartiliano16@gmail.com>
2024-05-17 21:50:44 -0300

    ajuste codigo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,5 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         byte idade = 123;
 		short ano = 2021;
 		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
@@ -13,6 +13,8 @@ public class TiposVariaveis {
 
         final double VALOR_DE_PI = 3.14;
 		
+        System.out.println(idade + ano + cep + cpf + pi + salario + numeroCurto2);
+        System.out.println(VALOR_DE_PI);
 		// VALOR_DE_PI=3.15; Esta linha vai apresentar erro de compilação!
     }
 }
