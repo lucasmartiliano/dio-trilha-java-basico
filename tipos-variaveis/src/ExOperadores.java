7b919a2c06853614e8f8568bfa5463e87777a04b
João Lucas Martiliano da Silva <lucasmartiliano16@gmail.com>
2024-05-17 22:01:53 -0300

    Operadores ex

---
+++ tipos-variaveis/src/ExOperadores.java
@@ -0,0 +1,29 @@
+public class ExOperadores {
+    public static void main(String[] args){
+        // double soma = 10.5 + 15.7;
+        // int subtração = 113 - 25;
+        // int multiplicacao = 20 * 7;
+        // int divisao = 15 / 3;
+        // int modulo = 18 % 3;
+        // double resultado = (10 * 7) + (20/4);
+
+        //classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+            System.out.println(nomeCompleto);
+
+        //qual o resultado das expressoes abaixo?
+        
+        String concatenacao ="?"; 
+
+            concatenacao = 1+1+1+"1";
+                System.out.println(concatenacao);                
+            concatenacao = 1+"1"+1+1;
+                System.out.println(concatenacao);        
+            concatenacao = 1+"1"+1+"1";
+                System.out.println(concatenacao);        
+            concatenacao = "1"+1+1+1;
+                System.out.println(concatenacao);        
+            concatenacao = "1"+(1+1+1);
+                System.out.println(concatenacao);
+    }        
+}
