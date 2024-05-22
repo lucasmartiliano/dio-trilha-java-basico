6d733415154cc86a9f8dd67e9c4146d530b500d8
João Lucas Martiliano da Silva <lucasmartiliano16@gmail.com>
2024-05-21 23:53:17 -0300

    metodo tvsmart

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,16 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Tv Ligada ? : " + smartTv.ligada);
+        System.out.println("Canal Atual ? : " + smartTv.canal);
+        System.out.println("Volume atual ? : " + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> Tv Ligada ? : " + smartTv.ligada);
+
+        smartTv.desligar ();
+        System.out.println("Novo Status -> Tv Ligada ? : " + smartTv.ligada);
+    }
+}
